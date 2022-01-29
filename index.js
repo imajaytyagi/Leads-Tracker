652f54ceb4f2d67e76c7268113fd3360dffe7346
Ajay Tyagi <imajaytyagi@gmail.com>
2022-01-30 01:11:47 +0530

    added deleteLeads functionality

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+// chrome://extensions/
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
+if(leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
     renderLeads()
-    
-})
-
+}
 
 function renderLeads() {
     let listItem = ""
@@ -25,3 +25,19 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItem
 }
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    renderLeads()
+})
+
+deleteBtn.addEventListener("dblclick",function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
+
+
+
