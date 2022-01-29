20d4c1932ce88fc59cd75f558d4f052c4c957f98
Ajay Tyagi <imajaytyagi@gmail.com>
2022-01-30 01:53:14 +0530

    final commit

--- index.js
+++ index.js
@@ -3,22 +3,31 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const tabBtn = document.getElementById("tab-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
-function renderLeads() {
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)             
+    })
+})
+
+function render(leads) {
     let listItem = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         // listItem += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>" 
         listItem += `
         <li>
-            <a target='_blank' href="${myLeads[i]}">
-                ${myLeads[i]}
+            <a target='_blank' href="${leads[i]}">
+                ${leads[i]}
             </a>
         </li>
         ` 
@@ -30,13 +39,13 @@ inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
 })
 
 deleteBtn.addEventListener("dblclick",function() {
     localStorage.clear()
     myLeads = []
-    renderLeads()
+    render(myLeads)
 })
 
 
